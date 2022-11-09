899dff6035faa518e1ecec9da92369ae28600f44
Matthew-Graves-Git <matthew.graves.cs@gmail.com>
2022-11-09 08:55:10 -0500

    project setup

---
+++ src/main/java/com/mattg/awsimagebucket/AwsImageBucketApplication.java
@@ -0,0 +1,13 @@
+package com.mattg.awsimagebucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageBucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mattg/awsimagebucket/AwsImageBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattg.awsimagebucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
