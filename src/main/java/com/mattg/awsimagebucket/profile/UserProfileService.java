dd2d65829c0725669d42332faf149f162e139d6b
Matthew Graves <matthew.graves.cs@gmail.com>
2022-11-11 09:24:31 -0500

    fixed service

--- src/main/java/com/mattg/awsimagebucket/profile/UserProfileService.java
+++ src/main/java/com/mattg/awsimagebucket/profile/UserProfileService.java
@@ -1,11 +1,12 @@
 package com.mattg.awsimagebucket.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
-
+@Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
